37e18818d3ab4892f4c7f5f23704f4e4088b3cbb
Jesse <jesseneumann@gmail.com>
2025-11-08 12:42:49 -0700

    fix: use correct base template

--- packages/libs/ic-js/src/index.ts
+++ packages/libs/ic-js/src/index.ts
@@ -2,6 +2,3 @@ export * from './api/index.js';
 export * from './icrc16.js';
 export * from './config.js';
 export * from './tokens.js';
-
-// We re-export declarations for easy access
-// Version bump for npm publish compatibility
